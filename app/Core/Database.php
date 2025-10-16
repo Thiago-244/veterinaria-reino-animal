772ec7716e81a24bcfe3e7fb59cbc7b56a3dfd6f
tsanchez-19 <thiagoenriquesanchezabad@gmail.com>
2025-10-16 01:56:04 -0500

    t

--- app/Core/Database.php
+++ app/Core/Database.php
@@ -5,31 +5,37 @@ use PDO;
 use PDOException;
 
 class Database {
-    private $host = DB_HOST;
-    private $user = DB_USER;
-    private $pass = DB_PASS;
-    private $dbname = DB_NAME;
-    private $charset = DB_CHARSET;
-
-    private $dbh; // Database Handler
-    private $stmt; // Statement
-    private $error;
+    private $dbh;
+    private $stmt;
 
     public function __construct() {
-        $dsn = "mysql:host={$this->host};dbname={$this->dbname};charset={$this->charset}";
+        $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_CHARSET;
         $options = [
             PDO::ATTR_PERSISTENT => true,
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Devuelve arrays asociativos
         ];
 
         try {
-            $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+            $this->dbh = new PDO($dsn, DB_USER, DB_PASS, $options);
         } catch (PDOException $e) {
-            $this->error = $e->getMessage();
-            echo $this->error;
+            die('Error de Conexión: ' . $e->getMessage());
         }
     }
 
-    // Aquí irán más métodos para hacer consultas
+    // Prepara la consulta
+    public function query($sql) {
+        $this->stmt = $this->dbh->prepare($sql);
+    }
+
+    // Ejecuta la consulta
+    public function execute() {
+        return $this->stmt->execute();
+    }
+
+    // Obtener todos los resultados
+    public function resultSet() {
+        $this->execute();
+        return $this->stmt->fetchAll();
+    }
 }
